be722ba652c5513dcd5f0f9bfa579a8c426d6f23
Andre Albuquerque <alalbuquerque22@gmail.com>
2022-05-18 20:19:06 -0300

    mock of googlesignin

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -7,6 +7,10 @@ import React from 'react';
 import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
+jest.mock('@react-native-google-signin/google-signin', () => {});
+
 jest.mock('redux-persist', () => {
   const real = jest.requireActual('redux-persist');
   return {
